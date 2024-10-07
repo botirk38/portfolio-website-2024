1d06c0d28cd4dd313cd976740e4a161efd938a38
Botir Khaltaev <ZLAC172@live.rhul.ac.uk>
2024-10-07 23:24:54 +0100

    Feat: Update to use my info@

--- app/components/work/projectDetails.ts
+++ app/components/work/projectDetails.ts
@@ -1,16 +1,24 @@
 import {
-    SiCplusplus,
-    SiFramer,
-    SiGithub, SiNeovim,
-    SiNextdotjs,
-    SiReact,
-    SiRust,
-    SiTailwindcss,
-    SiTypescript,
-    SiWebgl,
-    SiZig
+  SiCplusplus,
+  SiFramer,
+  SiGithub,
+  SiNeovim,
+  SiNextdotjs,
+  SiReact,
+  SiRust,
+  SiTailwindcss,
+  SiTypescript,
+  SiWebgl,
+  SiZig,
+  SiVite,
+  SiMysql,
+  SiPython,
+  SiOpenai,
 } from "react-icons/si";
-import {IconType} from "react-icons";
+
+import { DiJava } from "react-icons/di";
+
+import { IconType } from "react-icons";
 
 export type ProjectProps = {
   id: number;
@@ -26,30 +34,72 @@ export type ProjectProps = {
 };
 
 export const projects = [
-    {
-        id: 0,
-        name: "Eldora UI",
-        description:
-            "open-source animated components built with React, Typescript, Tailwind CSS, and Framer Motion. 100% open-source, and customizable.",
-        technologies: [SiTypescript, SiReact, SiNextdotjs, SiTailwindcss, SiFramer],
-        techNames: ["TypeScript", "React", "Next.js", "Tailwind CSS", "Framer Motion"],
-        techLinks: ["https://www.typescriptlang.org/", "https://reactjs.org/", "https://nextjs.org/", "https://tailwindcss.com/", "https://www.framer.com/motion/"],
-        github: "https://github.com/karthikmudunuri/eldoraui",
-        demo: "https://www.eldoraui.site/",
-        image: "/projects/eldoraui.png",
-        available: true,
-    },
-    {
-        id: 1,
-        name: "VR MALL",
-        description:
-            "Developed an virtual mall by using Three.js and Typescript..",
-        technologies: [SiWebgl,SiTypescript, SiReact],
-        techNames: ["WebGL", "Typescript", "React"],
-        techLinks: ["https://get.webgl.org/", "https://www.typescriptlang.org/", "https://reactjs.org/"],
-        github: "https://github.com/karthikmudunuri/VRMALL",
-        demo: "https://vrmalldemo.netlify.app/",
-        image: "/projects/vrmall.png",
-        available: true,
-    },
+  {
+    id: 0,
+    name: "StudentCopilot",
+    description:
+      "An all-in-one platform AI copilot for students to manage their academic life.",
+    technologies: [SiTypescript, SiReact, SiTailwindcss, SiVite, SiOpenai],
+    techNames: [
+      "TypeScript",
+      "React",
+      "Tailwind CSS",
+      "Vite",
+      "Convex",
+      "OpenAI",
+    ],
+    techLinks: [
+      "https://www.typescriptlang.org/",
+      "https://reactjs.org/",
+      "https://nextjs.org/",
+      "https://tailwindcss.com/",
+      "https://vitejs.dev/",
+      "https://convex.app/",
+      "https://openai.com/",
+    ],
+    github: "https://github.com/Egham-7/StudentCopilot",
+    demo: "https://www.studentcopilot.app/",
+    image: "/projects/StudentCopilot.png",
+    available: true,
+  },
+  {
+    id: 1,
+    name: "TruthLens",
+    description:
+      "Devlop a google chrome extension that uses LLMs to fact check multi modal information.",
+    technologies: [DiJava, SiMysql, SiReact, SiTypescript, SiOpenai],
+    techNames: ["Java", "MySQL", "Typescript", "React", "OpenAI"],
+    techLinks: [
+      "https://www.typescriptlang.org/",
+      "https://reactjs.org/",
+      "https://nextjs.org/",
+      "https://tailwindcss.com/",
+      "https://www.mysql.com/",
+      "https://openai.com/",
+    ],
+    github: "https://github.com/botirk38/fact-checker",
+    demo: "https://fact-checker-production.up.railway.app/",
+    image: "/projects/TruthLens.png",
+    available: true,
+  },
+
+  {
+    id: 2,
+    name: "StartupPlanner",
+    description:
+      "A all in one platform for young entrepreneurs to build their dream business. ",
+    technologies: [SiTypescript, SiReact, SiNextdotjs, SiPython, SiOpenai],
+    techNames: ["TypeScript", "React", "Next.js", "Python", "OpenAI"],
+    techLinks: [
+      "https://www.typescriptlang.org/",
+      "https://reactjs.org/",
+      "https://nextjs.org/",
+      "https://www.python.org/",
+      "https://openai.com/",
+    ],
+    github: "https://github.com/botirk38/startup-planner",
+    demo: "https://startup-planner.vercel.app/",
+    image: "/projects/StartupPlanner.png",
+    available: true,
+  },
 ];
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,82 +4,58 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 const syne = Inter({
-    subsets: ["latin"],
-    display: "block",
-    weight: ["400", "500", "600", "700", "800"],
+  subsets: ["latin"],
+  display: "block",
+  weight: ["400", "500", "600", "700", "800"],
 });
 
 export const metadata: Metadata = {
-    metadataBase: new URL("https://www.example.com/"),
-    title: "Portfolio template - Eldora UI",
-    description:
-    "Eldora UI is a portfolio template built with React, Typescript, Tailwind CSS, and Framer Motion. 100% open-source, and customizable.",
-    generator: "Next.js",
-    applicationName: "Portfolio template",
-    keywords: [
-        "Portfolio template",
-        "Eldora UI",
-        "React",
-        "developer",
-        "frontend",
-        "nextjs",
-        "react",
-        "frontend developer",
-        "frontend engineer",
-        "creative",
-        "creative developer",
-        "creative engineer",
-        "tech",
-        "software",
-        "software developer",
-        "portfolio",
-        "frontend developer portfolio",
-        "creative developer portfolio",
-        "creative engineer portfolio",
-        "software developer portfolio",
-        "frontend engineer portfolio",
-    ],
-    colorScheme: "dark",
-    openGraph: {
-        title: "Portfolio template - Eldora UI",
-        description:
-      "Eldora UI is a portfolio template built with React, Typescript, Tailwind CSS, and Framer Motion. 100% open-source, and customizable.",
-        url: "https://www.eldoraui.site/",
-        siteName: "www.eldoraui.site",
-        images: [
-            {
-                url: "./public/metadata.jpg",
-                width: 1200,
-                height: 630,
-                alt: "Portfolio template - Eldora UI",
-            },
-        ],
-        locale: "en-US",
-        type: "website",
+  metadataBase: new URL("https://www.example.com/"),
+  title: "Botir Khaltaev - Portfolio",
+  description: "A portfolio website for Botir Khaltaev",
+  generator: "Next.js",
+  applicationName: "Portfolio template",
+  keywords: [
+    "Portfolio template",
+    "React",
+    "developer",
+    "backend",
+    "nextjs",
+    "react",
+    "rust",
+    "typescript",
+    "cplusplus",
+    "tailwindcss",
+    "java",
+    "meta",
+    "facebook",
+    "backend developer",
+    "backend engineer",
+    "creative",
+    "creative developer",
+    "creative engineer",
+    "tech",
+    "software",
+    "software developer",
+    "portfolio",
+    "software developer portfolio",
+    "backend engineer portfolio",
+  ],
+  colorScheme: "dark",
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
     },
-    twitter: {
-        card: "summary_large_image",
-        title: "Portfolio template - Eldora UI",
-        description:
-      "Portfolio template - Eldora UI is a portfolio template built with React, Typescript, Tailwind CSS, and Framer Motion. 100% open-source, and customizable.",
-        creator: "@karthikmudunuri",
-        creatorId: "0000000000",
-        images: ["./public/metadata.jpg"],
-    },
-    robots: {
-        index: true,
-        follow: true,
-        nocache: false,
-        googleBot: {
-            index: true,
-            follow: false,
-            noimageindex: true,
-            "max-video-preview": -1,
-            "max-image-preview": "large",
-            "max-snippet": -1,
-        },
-    },
-    category: "technology",
+  },
+  category: "technology",
 };
 
 type RootLayoutProps = {
@@ -87,13 +63,13 @@ type RootLayoutProps = {
 };
 
 export default function RootLayout({ children }: RootLayoutProps) {
-    return (
-        <html lang="en">
-            <body
-                className={`${syne.className} scroll-smooth scrollbar-none scrollbar-track-[#0E1016] scrollbar-thumb-[#212531]`}
-            >
-                {children}
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body
+        className={`${syne.className} scroll-smooth scrollbar-none scrollbar-track-[#0E1016] scrollbar-thumb-[#212531]`}
+      >
+        {children}
+      </body>
+    </html>
+  );
 }
